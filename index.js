5572bf062af72610ac6b6846a88c50bd2e27e87d
Ali Bas <alibas01@gmail.com>
2021-01-10 20:24:46 -0500

    function created successfully

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-function sumItems(array) {
+const sumItems = function(array) {
   // Sum all the numbers in the array
-}
+  let sum = 0
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      sum += sumItems(array[i]);
+    } else {
+      sum += array[i];
+    }
+  }
+  return sum;
+};
 
 module.exports = sumItems;
